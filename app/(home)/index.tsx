880ac28d4b9bb00439793b1350aba991d05a9c94
ujjwal.aggarwal <ujjwal.aggarwal@geekyants.com>
2024-10-18 13:15:43 +0530

    feat/addBlankScreenToChatAndExplore

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const index = () => {
   return (
-    <View style={{flex:1}}>
-      <Text>index</Text>
-    </View>
+    <SafeAreaView style={{flex:1,backgroundColor:'rgb(40,25,52)',justifyContent: 'center',alignItems:'center'}}>
+      <Text className='text-3xl text-white'>Home Explore Screen</Text>
+    </SafeAreaView>
   )
 }
 
