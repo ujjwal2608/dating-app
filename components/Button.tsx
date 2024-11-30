c1a7a9443e8074e94fdcbf764c7789699b2bdf4d
ujjwal2608 <98085611+ujjwal2608@users.noreply.github.com>
2024-11-30 09:22:53 -0800

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -19,6 +19,6 @@ export const Button = forwardRef<TouchableOpacity, ButtonProps>(
 );
 
 const styles = {
-  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4',
+  button: 'items-center bg-indigo-600 rounded-[28px] shadow-md p-4',
   buttonText: 'text-white text-lg font-semibold text-center',
 };
